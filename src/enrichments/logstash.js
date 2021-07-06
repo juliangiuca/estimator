de41e4d7b1b2fe7e3a836e87c09e548e81513080
Julian Giuca <julian@newrelic.com>
2021-07-05 18:25:35 -0700

    add logstash

---
+++ src/enrichments/logstash.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "@timestamp": "2021-07-06T01:22:38.195Z",  
+  "@version": "1",
+  "host": "ip-10-146-79-93",
+  "path": "/home/ubuntu/logginator/destination/access.log",
+  "plugin.type": "logstash",
+  "plugin.version": "1.2.2",
+}
